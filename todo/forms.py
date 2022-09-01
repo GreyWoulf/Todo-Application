02496cf03d11ef127423458aa2e00d3337dab340
GreyWoulf1 <93624560+GreyWoulf@users.noreply.github.com>
2022-09-01 21:59:38 +0000

    Fixed compiling errors and manually added in bootstrap files

--- todo/forms.py
+++ todo/forms.py
@@ -2,12 +2,18 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+from .models import TodoItem
+
 UserModel = get_user_model()
-
-
 
 class UserRegistrationForm(UserCreationForm):
 
-  class Meta:
-    model = UserModel
-    fields = ["username", "password1", "password2"]
+    class Meta:
+        model = UserModel
+        fields = ["username", "password1", "password2"]
+
+
+class TodoForm(forms.ModelForm):
+
+    class Meta:
+        fields = "__all__"
